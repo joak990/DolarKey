428a38bc636f8a46fa5de5e9f172f5b1681ea02d
joak990 <joakhaidar@gmail.com>
2023-06-28 20:14:14 -0300

    dada

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,17 +7,7 @@ function Nav() {
         <div className="text-white mt-4 text-2xl font-bold">dolarkey</div>
         <div className="text-black font-bold">a tu alcance</div>
       </div>
-      <div className="container mx-auto ">
-        <ul className="flex justify-center items-center gap-20 space-x-4">
-          <li>
-            <a href="#" className="text-white font-medium hover:text-gray-300">Home</a>
-          </li>
-         
-          <li>
-            <a href="#" className="text-white font-medium hover:text-gray-300">Sobre nosotros</a>
-          </li>
-        </ul>
-      </div>
+     
     </nav>
   );
 }
