722e9f986cb52506f502cebf09fde6fb732528d1
joak990 <joakhaidar@gmail.com>
2023-06-30 00:25:23 -0300

    ad

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -2,9 +2,9 @@
 import Swal from 'sweetalert2';
 import axios from "axios"
 import { GET_DOLAR } from "./Types";
-const url100= "https://www.mercadopago.com.ar/checkout/v1/payment/redirect?preference-id=605411115-d9958e62-77cb-4342-af05-dfd624093eaa&router-request-id=1d738b2a-fe05-4861-a793-a9bfc640a455"
-const url300="https://www.mercadopago.com.ar/checkout/v1/payment/redirect/sniffing?sniffing-rollout=sniffing-api&router-request-id=324d6402-530c-40e2-abc4-dbc38a376a06&preference-id=605411115-663f1bec-0c6b-42b9-b6c4-23a142bac365"
-const url500="https://www.mercadopago.com.ar/checkout/v1/payment/redirect/c1a90141-6013-400c-a68c-a4eb4744ca60/payment-option-form/?preference-id=605411115-71a409d3-fb8e-40fc-ac67-516ee7791bb5&router-request-id=eaaf9293-d7c3-40f4-9782-951a1a4ee640&p=931461430409e758e950c7a60b9e975b#/"
+const url100= "http://mpago.la/15x4rJf"
+const url300="http://mpago.la/2TZEYAa"
+const url500="http://mpago.la/1NyhESg"
 export function getalldollars() {
     return function (dispatch) {
       axios
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -66,134 +66,131 @@ const handleSubmit = (event) => {
 };
 let ultimoValorVariacion = 0;
 
-  return (
-    <div className="flex justify-center items-center flex-col min-h-screen bg-slate-300">
-      <h1 className="font-bold text-4xl mt-5 ml-7">Precio del DÓLAR HOY en Argentina</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 max-w-4xl">
-        <div className="p-4 bg-gray-200 rounded-lg">
-        <h2 className="font-bold text-xl text-center mb-2">Dólar blue</h2>
-          <div className="flex justify-between items-center">
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Compra:</p>
-              <p className="text-green-700 text-2xl font-bold">${data[1]?.compra}
-          </p>
-            </div>
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Venta:</p>
-              <p className="text-red-700 text-2xl font-bold">${data[1]?.venta}</p>
-            </div>
+return (
+  <div className="container mx-auto py-10 bg-gray-100">
+    <h1 className="text-4xl font-bold text-center mb-10">Precio del DÓLAR HOY en Argentina</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl mx-auto">
+      <div className="p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-xl font-bold text-center mb-2">Dólar blue</h2>
+        <div className="flex justify-between items-center">
+          <div className="text-center">
+            <p className="text-gray-600">Compra:</p>
+            <p className="text-2xl font-bold text-green-700">${data[1]?.compra}</p>
           </div>
-        </div>
-        <div className="p-4 bg-gray-200 rounded-lg">
-          <h2 className="font-bold text-xl text-center mb-2">Dólar Oficial</h2>
-          <div className="flex justify-between items-center">
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Compra:</p>
-              <p className="text-green-700 text-2xl font-bold">${data[0]?.compra}</p>
-            </div>
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Venta:</p>
-              <p className="text-red-700 text-2xl font-bold">${data[0]?.venta}</p>
-            </div>
-          </div>
-        </div>
-        <div className="p-4 bg-gray-200 rounded-lg">
-          <h2 className="font-bold text-xl text-center mb-2">Dólar contado con liqui</h2>
-          <div className="flex justify-between items-center">
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Compra:</p>
-              <p className="text-green-700 text-2xl font-bold">${data[3]?.compra}</p>
-            </div>
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Venta:</p>
-              <p className="text-red-700 text-2xl font-bold">${data[3]?.venta}</p>
-            </div>
-          </div>
-        </div>
-        <div className="p-4 bg-gray-200 rounded-lg">
-          <h2 className="font-bold text-xl text-center mb-2">Dólar Turista</h2>
-          <div className="flex justify-between items-center">
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Compra:</p>
-              <p className="text-green-700 text-2xl font-bold">${data[4]?.compra}</p>
-            </div>
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Venta:</p>
-              <p className="text-red-700 text-2xl font-bold">${data[4]?.venta}</p>
-            </div>
-          </div>
-         
-        </div>
-        <div className="p-4 bg-gray-200 rounded-lg">
-          <h2 className="font-bold text-xl text-center mb-2">Dólar bolsa</h2>
-          <div className="flex justify-between items-center">
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Compra:</p>
-              <p className="text-green-700 text-2xl font-bold">${data[2]?.compra}</p>
-            </div>
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Venta:</p>
-              <p className="text-red-700 text-2xl font-bold">${data[2]?.venta}</p>
-            </div>
-          </div>
-        </div>
-        <div className="p-4 bg-gray-200 rounded-lg">
-          <h2 className="font-bold text-xl text-center mb-2">Dólar mayorista</h2>
-          <div className="flex justify-between items-center">
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Compra:</p>
-              <p className="text-green-700 text-2xl font-bold">${data[5]?.compra}</p>
-            </div>
-          
-            <div className="text-center">
-              <p className="font-bold text-gray-600">Venta:</p>
-              <p className="text-red-700 text-2xl font-bold">${data[5]?.venta}</p>
-            </div>
+          <div className="text-center">
+            <p className="text-gray-600">Venta:</p>
+            <p className="text-2xl font-bold text-red-700">${data[1]?.venta}</p>
           </div>
         </div>
       </div>
-      <h2 className="text-2xl font-light mt-8 ml-6">¿Te gustaría recibir notificaciones del movimiento en tiempo real del dólar blue?✔️</h2>
-      <h1 className='font-bold mt-16 text-2xl mr-4'>NEWSLETTER</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row mt-4">
-        <div className="flex flex-col sm:flex-row sm:items-center">
-          <label htmlFor="name" className="text-lg font-bold mb-2 mr-2">
-            Nombre:
-          </label>
-          <input
-            value={form.nombre}
-            type="text"
-            id="nombre"
-            name="nombre"
-            onChange={handleInputChange}
-            className="border border-gray-400 p-2 rounded"
-            placeholder="Ingrese su nombre"
-          />
-          {errors.nombre && <p className="text-red-500 text-sm mt-1">{errors.nombre}</p>}
+      <div className="p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-xl font-bold text-center mb-2">Dólar Oficial</h2>
+        <div className="flex justify-between items-center">
+          <div className="text-center">
+            <p className="text-gray-600">Compra:</p>
+            <p className="text-2xl font-bold text-green-700">${data[0]?.compra}</p>
+          </div>
+          <div className="text-center">
+            <p className="text-gray-600">Venta:</p>
+            <p className="text-2xl font-bold text-red-700">${data[0]?.venta}</p>
+          </div>
         </div>
-        <div className="flex flex-col sm:flex-row sm:items-center mt-4 sm:mt-0">
-          <label htmlFor="email" className="text-lg font-bold mb-2 ml-4 mr-2">
-            Email:
-          </label>
-          <input
-            value={form.email}
-            type="email"
-            id="email"
-            name="email"
-            onChange={handleInputChange}
-            className="border border-gray-400 p-2 rounded"
-            placeholder="Ingrese su email"
-          />
-          {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+      </div>
+      <div className="p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-xl font-bold text-center mb-2">Dólar contado con liqui</h2>
+        <div className="flex justify-between items-center">
+          <div className="text-center">
+            <p className="text-gray-600">Compra:</p>
+            <p className="text-2xl font-bold text-green-700">${data[3]?.compra}</p>
+          </div>
+          <div className="text-center">
+            <p className="text-gray-600">Venta:</p>
+            <p className="text-2xl font-bold text-red-700">${data[3]?.venta}</p>
+          </div>
         </div>
-        <button
-          type="submit"
-          className=" mt-5 md:ml-4 md:mt-0 bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-        >
-          Suscribirse
-        </button>
-      </form>
+      </div>
+      <div className="p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-xl font-bold text-center mb-2">Dólar Turista</h2>
+        <div className="flex justify-between items-center">
+          <div className="text-center">
+            <p className="text-gray-600">Compra:</p>
+            <p className="text-2xl font-bold text-green-700">${data[4]?.compra}</p>
+          </div>
+          <div className="text-center">
+            <p className="text-gray-600">Venta:</p>
+            <p className="text-2xl font-bold text-red-700">${data[4]?.venta}</p>
+          </div>
+        </div>
+      </div>
+      <div className="p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-xl font-bold text-center mb-2">Dólar bolsa</h2>
+        <div className="flex justify-between items-center">
+          <div className="text-center">
+            <p className="text-gray-600">Compra:</p>
+            <p className="text-2xl font-bold text-green-700">${data[2]?.compra}</p>
+          </div>
+          <div className="text-center">
+            <p className="text-gray-600">Venta:</p>
+            <p className="text-2xl font-bold text-red-700">${data[2]?.venta}</p>
+          </div>
+        </div>
+      </div>
+      <div className="p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-xl font-bold text-center mb-2">Dólar mayorista</h2>
+        <div className="flex justify-between items-center">
+          <div className="text-center">
+            <p className="text-gray-600">Compra:</p>
+            <p className="text-2xl font-bold text-green-700">${data[5]?.compra}</p>
+          </div>
+          <div className="text-center">
+            <p className="text-gray-600">Venta:</p>
+            <p className="text-2xl font-bold text-red-700">${data[5]?.venta}</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    <h2 className="text-2xl font-light mt-8 text-center">¿Te gustaría recibir notificaciones del movimiento en tiempo real del dólar blue?</h2>
+    <h1 className="font-bold mt-16 text-2xl text-center">NEWSLETTER</h1>
+    <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row mt-4 justify-center items-center">
+      <div className="flex flex-col sm:flex-row sm:items-center">
+        <label htmlFor="name" className="text-lg font-bold mb-2">
+          Nombre:
+        </label>
+        <input
+          value={form.nombre}
+          type="text"
+          id="nombre"
+          name="nombre"
+          onChange={handleInputChange}
+          className="border border-gray-400 p-2 rounded"
+          placeholder="Ingrese su nombre"
+        />
+        {errors.nombre && <p className="text-red-500 text-sm mt-1">{errors.nombre}</p>}
+      </div>
+      <div className="flex flex-col sm:flex-row sm:items-center mt-4 sm:mt-0">
+        <label htmlFor="email" className="text-lg font-bold mb-2 ml-4">
+          Email:
+        </label>
+        <input
+          value={form.email}
+          type="email"
+          id="email"
+          name="email"
+          onChange={handleInputChange}
+          className="border border-gray-400 p-2 rounded"
+          placeholder="Ingrese su email"
+        />
+        {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+      </div>
+      <button
+        type="submit"
+        className="mt-5 bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+      >
+        Suscribirse
+      </button>
+    </form>
+  </div>
+);
 }
 
 export default Home;
